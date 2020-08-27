1cb9e3cc4766acf5c72ee5a7f3f49a0059b2540c
Pedro alfaro <guerrero.03@outlook.com>
2020-08-27 12:04:24 -0400

    generar un hola mundo
    generar colores
    
    agregar textview y button

--- fuentes/CalculadoraApp/app/src/main/java/cl/Inacap/CalculadoraApp/MainActivity.java
+++ fuentes/CalculadoraApp/app/src/main/java/cl/Inacap/CalculadoraApp/MainActivity.java
@@ -3,6 +3,7 @@ package cl.Inacap.CalculadoraApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Saludar(View v){
+        Toast.makeText( context:this, Text:"hola pedro", Toast.LENGTH_LONG).show();
     }
 }
